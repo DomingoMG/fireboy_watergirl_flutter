04c54a112102a47f8c3fb133282924b5439e6582
DomingoMG <domingomg97@gmail.com>
2025-03-02 14:24:49 +0000

    Release: v0.0.3

--- lib/misc/water_pool_hitbox.dart
+++ lib/misc/water_pool_hitbox.dart
@@ -1,11 +1,9 @@
-import 'package:fireboy_and_watergirl/config/enums/hitbox_type.dart';
 import 'package:flame/components.dart';
 import 'package:flame/collisions.dart';
 import 'package:flame/events.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:fireboy_and_watergirl/misc/misc.dart';
 import 'package:fireboy_and_watergirl/characters/fireboy/fireboy.dart';
+import 'package:fireboy_and_watergirl/config/enums/hitbox_type.dart';
 import 'package:fireboy_and_watergirl/fireboy_and_watergirl_game.dart';
 import 'package:fireboy_and_watergirl/characters/watergirl/watergirl.dart';
 
@@ -74,48 +72,48 @@ class WaterPoolHitbox extends SpriteAnimationComponent with CollisionCallbacks, 
   }
 
 
-  @override
-  void onDragStart(DragStartEvent event) {
-    if( !kDebugMode ) return;
-    Vector2 localPosition = event.localPosition;
+  // @override
+  // void onDragStart(DragStartEvent event) {
+  //   if( !kDebugMode ) return;
+  //   Vector2 localPosition = event.localPosition;
 
-    // Verificar si el usuario está agarrando la esquina inferior derecha
-    if ((localPosition.x >= size.x - resizeThreshold && localPosition.y >= size.y - resizeThreshold)) {
-      isResizing = true;
-    }
-    super.onDragStart(event);
-  }
+  //   // Verificar si el usuario está agarrando la esquina inferior derecha
+  //   if ((localPosition.x >= size.x - resizeThreshold && localPosition.y >= size.y - resizeThreshold)) {
+  //     isResizing = true;
+  //   }
+  //   super.onDragStart(event);
+  // }
 
-  @override
-  void onDragUpdate(DragUpdateEvent event) {
-   if( !kDebugMode ) return;
-   if (isResizing) {
-      // Redimensionar sin que se invierta
-      size += event.localDelta;
-      size.clamp(Vector2(20, 20), Vector2(500, 500)); // Define un tamaño mínimo y máximo
-    } else {
-      // Mueve el bloque si no está en modo redimensionamiento
-      position += event.localDelta;
-    }
-    super.onDragUpdate(event);
-  }
+  // @override
+  // void onDragUpdate(DragUpdateEvent event) {
+  //  if( !kDebugMode ) return;
+  //  if (isResizing) {
+  //     // Redimensionar sin que se invierta
+  //     size += event.localDelta;
+  //     size.clamp(Vector2(20, 20), Vector2(500, 500)); // Define un tamaño mínimo y máximo
+  //   } else {
+  //     // Mueve el bloque si no está en modo redimensionamiento
+  //     position += event.localDelta;
+  //   }
+  //   super.onDragUpdate(event);
+  // }
 
-  @override
-  void onDragEnd(DragEndEvent event) {
-    if( !kDebugMode ) return;
-    isResizing = false;
-    debugPrint('Water pool position: x=${position.x}, y=${position.y}, Size: w=${size.x}, h=${size.y}');
-    super.onDragEnd(event);
-  }
+  // @override
+  // void onDragEnd(DragEndEvent event) {
+  //   if( !kDebugMode ) return;
+  //   isResizing = false;
+  //   debugPrint('Water pool position: x=${position.x}, y=${position.y}, Size: w=${size.x}, h=${size.y}');
+  //   super.onDragEnd(event);
+  // }
 
-  @override
-  void render(Canvas canvas) {
-    if( !kDebugMode ) return;
-    final paint = Paint()..color = Colors.red.withValues(alpha: 0.5);
-    canvas.drawRect(
-      Rect.fromLTWH(size.x - resizeThreshold, size.y - resizeThreshold, resizeThreshold, resizeThreshold),
-      paint,
-    );
-    super.render(canvas);
-  }
+  // @override
+  // void render(Canvas canvas) {
+  //   if( !kDebugMode ) return;
+  //   final paint = Paint()..color = Colors.red.withValues(alpha: 0.5);
+  //   canvas.drawRect(
+  //     Rect.fromLTWH(size.x - resizeThreshold, size.y - resizeThreshold, resizeThreshold, resizeThreshold),
+  //     paint,
+  //   );
+  //   super.render(canvas);
+  // }
 }
