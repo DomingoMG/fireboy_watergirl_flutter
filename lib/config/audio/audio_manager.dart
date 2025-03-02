f2cb96a61f2f4a3105dd212fc30ceafc788073b1
DomingoMG <domingomg97@gmail.com>
2025-03-02 11:49:55 +0000

    Made playSound asynchronous and adjusted music volume levels

--- lib/config/audio/audio_manager.dart
+++ lib/config/audio/audio_manager.dart
@@ -11,17 +11,17 @@ class AudioManager {
     _initialized = true;
   }
 
-  static void playSound(AudioType audio, {double volume = 1.0, loop = false}) {
+  static Future<void> playSound(AudioType audio, {double volume = 1.0, loop = false}) async {
     if( loop ) {
       FlameAudio.loop(audio.assetName);
       return;
     }
     
-    FlameAudio.play(audio.assetName, volume: volume);
+    await FlameAudio.play(audio.assetName, volume: volume);
   }
 
   static void playMusicLevel() async {
-    await FlameAudio.bgm.play(AudioType.musicLevel.assetName, volume: 0.5);
+    await FlameAudio.bgm.play(AudioType.musicLevel.assetName, volume: 0.2);
   }
 
   static void stopMusicLevel() async {
@@ -29,7 +29,7 @@ class AudioManager {
   }
 
   static void playIntroMusic() async {
-    await FlameAudio.bgm.play(AudioType.musicIntro.assetName, volume: 0.5);
+    await FlameAudio.bgm.play(AudioType.musicIntro.assetName, volume: 0.2);
   }
 
   static void stopPlayIntroMusic() async {
