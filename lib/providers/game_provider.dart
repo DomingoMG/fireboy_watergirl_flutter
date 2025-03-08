98e8254b1f39777b42e2961c7d4f5e325f5a87c4
DomingoMG <domingomg97@gmail.com>
2025-03-08 20:03:10 +0000

    Feat: Add GameProvider with AsyncNotifier for game state management

---
+++ lib/providers/game_provider.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fireboy_and_watergirl/providers/socket_provider.dart';
+import 'package:fireboy_and_watergirl/config/sockets/models/game_model.dart';
+
+final providerGameStart = AsyncNotifierProvider<GameNotifier, GameStartModel>(GameNotifier.new);
+
+class GameNotifier extends AsyncNotifier<GameStartModel> {
+  @override
+  Future<GameStartModel> build() {
+    final socketRepository = ref.read(providerSocketRepository);
+    socketRepository.on('gameStart', (gameJson) {
+      state = const AsyncLoading();
+      if( gameJson is! Map<String, dynamic> ) return;
+      state = AsyncData(GameStartModel.fromJson(gameJson));
+    });
+
+    ref.onDispose(() => socketRepository.off('gameStart'));
+    return Future.value(GameStartModel(
+      lobbyId: '', 
+      players: []
+    ));
+  }
+
+  void createLobby( GameStartModel gameStart ) {
+    state = const AsyncLoading();
+    state = AsyncData(gameStart);
+  }
+}
