aab5768cf4eba0ee44dc5f49b12abfb7d378975a
DomingoMG <domingomg97@gmail.com>
2025-03-08 19:59:21 +0000

    Refactor: Rename MainMenu to MainMenuOverlay and add new overlays (LobbyMenuOverlay, WaitingPlayerOverlay)

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
+import 'package:fireboy_and_watergirl/overlays/waiting_player.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-import 'package:fireboy_and_watergirl/main_menu.dart';
+import 'package:fireboy_and_watergirl/overlays/lobby_menu.dart';
+import 'package:fireboy_and_watergirl/overlays/main_menu.dart';
 import 'package:fireboy_and_watergirl/fireboy_and_watergirl_game.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(GameWidget(
-    game: FireBoyAndWaterGirlGame(),
-    overlayBuilderMap: {
-      MainMenu.pathRoute: (context, game) => MainMenu(game: game as FireBoyAndWaterGirlGame),
-    },
-    initialActiveOverlays: const [MainMenu.pathRoute],
+  runApp(ProviderScope(
+    child: MaterialApp(
+      home: GameWidget(
+        game: FireBoyAndWaterGirlGame(),
+        overlayBuilderMap: {
+          MainMenuOverlay.pathRoute: (context, game) => MainMenuOverlay(game: game as FireBoyAndWaterGirlGame),
+          LobbyMenuOverlay.pathRoute: (context, game) => LobbyMenuOverlay(game: game as FireBoyAndWaterGirlGame),
+          WaitingPlayerOverlay.pathRoute: (context, game) => WaitingPlayerOverlay(game: game as FireBoyAndWaterGirlGame),
+        },
+        initialActiveOverlays: const [MainMenuOverlay.pathRoute],
+      ),
+    ),
   ));
 }
