88a9abe794fd4276715528b9802c2e27e7895f9f
Domingo <domingomg97@gmail.com>
2025-03-09 13:57:03 +0000

    Refactored main.dart to use RiverpodAwareGameWidget and global game instance for better state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
-import 'package:fireboy_and_watergirl/overlays/waiting_player.dart';
-import 'package:flame/game.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flame_riverpod/flame_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:fireboy_and_watergirl/overlays/lobby_menu.dart';
+import 'package:fireboy_and_watergirl/overlays/waiting_player.dart';
 import 'package:fireboy_and_watergirl/overlays/main_menu.dart';
 import 'package:fireboy_and_watergirl/fireboy_and_watergirl_game.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final gameInstance = FireBoyAndWaterGirlGame();
+final gameWidgetKey = GlobalKey<RiverpodAwareGameWidgetState>();
 
 void main() {
   runApp(ProviderScope(
     child: MaterialApp(
-      home: GameWidget(
-        game: FireBoyAndWaterGirlGame(),
+      home: RiverpodAwareGameWidget(
+        key: gameWidgetKey,
+        game: gameInstance,
         overlayBuilderMap: {
           MainMenuOverlay.pathRoute: (context, game) => MainMenuOverlay(game: game as FireBoyAndWaterGirlGame),
           LobbyMenuOverlay.pathRoute: (context, game) => LobbyMenuOverlay(game: game as FireBoyAndWaterGirlGame),
