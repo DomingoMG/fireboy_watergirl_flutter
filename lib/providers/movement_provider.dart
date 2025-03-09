3d39cc76b782b150dd5c85da724de0513267a10f
Domingo <domingomg97@gmail.com>
2025-03-09 18:40:04 +0000

    Implemented PlayerMovementNotifier to handle player position updates via sockets.

---
+++ lib/providers/movement_provider.dart
@@ -0,0 +1,58 @@
+import 'package:fireboy_and_watergirl/providers/game_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+import 'package:fireboy_and_watergirl/providers/socket_provider.dart';
+import 'package:fireboy_and_watergirl/config/sockets/models/player_movement.dart';
+
+final providerPlayerMovement = NotifierProvider<PlayerMovementNotifier, PlayerMoveModel>(
+  PlayerMovementNotifier.new
+);
+
+class PlayerMovementNotifier extends Notifier<PlayerMoveModel> {
+  @override
+  PlayerMoveModel build() {
+    final socketRepository = ref.read(providerSocketRepository);
+
+    socketRepository.on('updatePlayer', (updatePlayerJson) {
+      final playerMove = PlayerMoveModel.fromJson(updatePlayerJson);
+
+      debugPrint('✅ Se ha movido el jugador ${playerMove.playerId} a la posición ${playerMove.position.x}, ${playerMove.position.y}');
+
+      // 🔥 Usamos `copyWith` para actualizar solo valores nuevos
+      state = state.copyWith(
+        character: playerMove.character,
+        playerId: playerMove.playerId,
+        lobbyId: playerMove.lobbyId,
+        action: playerMove.action,
+        position: playerMove.position,
+      );
+    });
+
+    return PlayerMoveModel(
+      playerId: '',
+      lobbyId: '',
+      character: '',
+      action: PlayerMovementAction.idle,
+      position: PlayerPositionModel(x: 0, y: 0),
+    );
+  }
+
+  // ✅ Método para enviar movimiento al servidor
+  void sendMove(PlayerMovementAction action, double x, double y) {
+    final gameStart = ref.read(providerGameStart).value;
+    if( gameStart == null ) return;
+    
+    final socketRepository = ref.read(providerSocketRepository);
+    final movement = PlayerMoveModel(
+      character: state.character,
+      playerId: state.playerId,
+      lobbyId: state.lobbyId,
+      action: action,
+      position: PlayerPositionModel(x: x, y: y),
+    );
+
+    socketRepository.emit('playerMove', movement.toJson());
+
+    debugPrint("🎮 Enviando movimiento: ${movement.action.name} en ($x, $y)");
+  }
+}
