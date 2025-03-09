2dd478f7869ed82da34660016cf53d14bc6d7803
Domingo <domingomg97@gmail.com>
2025-03-09 14:01:58 +0000

    Integrated RiverpodComponentMixin into FireboyAnimation for state management enhancements.

--- lib/characters/fireboy/fireboy.dart
+++ lib/characters/fireboy/fireboy.dart
@@ -1,17 +1,17 @@
 import 'package:flame/components.dart';
+import 'package:flame_riverpod/flame_riverpod.dart';
 import 'package:flutter/services.dart' show LogicalKeyboardKey;
 import 'package:fireboy_and_watergirl/characters/character_animation.dart';
 import 'package:fireboy_and_watergirl/config/config.dart';
 
-class FireboyAnimation extends CharacterAnimation {
+class FireboyAnimation extends CharacterAnimation with RiverpodComponentMixin {
   FireboyAnimation() : super(
     characterType: 'fireboy',
     anchor: Anchor.center,
     size: Vector2(64, 64),
     position: Vector2(53, 650)
   );
-  
-  
+
   @override
   void jumpFromKeyEvent(Set<LogicalKeyboardKey> keysPressed) {
     if (keysPressed.contains(LogicalKeyboardKey.keyW)) {
