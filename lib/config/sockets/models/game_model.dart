fd767f0c1a36793e0d1f7242c5343b22dcb648ff
DomingoMG <domingomg97@gmail.com>
2025-03-09 11:31:04 +0000

    Feat: Add isGameStarted flag and new JSON factories in GameStartModel

--- lib/config/sockets/models/game_model.dart
+++ lib/config/sockets/models/game_model.dart
@@ -3,13 +3,24 @@ import 'package:fireboy_and_watergirl/config/sockets/models/player.dart';
 class GameStartModel {
   String lobbyId;
   List<PlayerModel> players;
+  bool isGameStarted;
 
   GameStartModel({
     required this.lobbyId, 
-    required this.players
+    required this.players,
+    this.isGameStarted = false
   });
 
-  factory GameStartModel.fromJson(Map<String, dynamic> json) {
+  factory GameStartModel.fromGameStartJson(Map<String, dynamic> json) {
+    return GameStartModel(
+      lobbyId: json['lobbyId'],
+      players: (json['players'] as List).map((p) => PlayerModel.fromJson(p)).toList(),
+      isGameStarted: true
+    );
+  }
+
+
+  factory GameStartModel.fromLobbyCreatedJson(Map<String, dynamic> json) {
     return GameStartModel(
       lobbyId: json['lobbyId'],
       players: (json['players'] as List).map((p) => PlayerModel.fromGameJson(p)).toList(),
