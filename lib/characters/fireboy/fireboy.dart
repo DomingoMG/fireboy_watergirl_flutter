3b123ed9d2f8fb6398a3afbe8531a01e44162b05
DomingoMG <domingomg97@gmail.com>
2025-03-09 18:52:56 +0000

    Integrated movement provider to synchronize player actions over the network

--- lib/characters/fireboy/fireboy.dart
+++ lib/characters/fireboy/fireboy.dart
@@ -1,6 +1,8 @@
 import 'package:flame/components.dart';
 import 'package:flutter/services.dart' show LogicalKeyboardKey;
 import 'package:fireboy_and_watergirl/characters/character_animation.dart';
+import 'package:fireboy_and_watergirl/config/sockets/models/player_movement.dart';
+import 'package:fireboy_and_watergirl/providers/movement_provider.dart';
 
 class FireboyAnimation extends CharacterAnimation {
   FireboyAnimation() : super(
@@ -12,21 +14,27 @@ class FireboyAnimation extends CharacterAnimation {
 
   @override
   void jumpFromKeyEvent(Set<LogicalKeyboardKey> keysPressed) {
+    final playerMovementController = ref.read(providerPlayerMovement.notifier);
     if (keysPressed.contains(LogicalKeyboardKey.keyW)) {
       jump();
+      playerMovementController.sendMove(PlayerMovementAction.jump, position.x, position.y);
     }
   }
 
  @override
   void moveFromKeyEvent(Set<LogicalKeyboardKey> keysPressed) {
+    final playerMovementController = ref.read(providerPlayerMovement.notifier);
     if (keysPressed.contains(LogicalKeyboardKey.keyA)) {
       animation = walkLeftAnimation;
       velocity.x = -speed;
+      playerMovementController.sendMove(PlayerMovementAction.moveLeft, position.x, position.y);
     } else if (keysPressed.contains(LogicalKeyboardKey.keyD)) {
       animation = walkRightAnimation;
       velocity.x = speed;
+      playerMovementController.sendMove(PlayerMovementAction.moveRight, position.x, position.y);
     } else {
       animation = idleAnimation;
+      playerMovementController.sendMove(PlayerMovementAction.idle, position.x, position.y);
     }
   }
 }
--- lib/characters/watergirl/watergirl.dart
+++ lib/characters/watergirl/watergirl.dart
@@ -1,3 +1,5 @@
+import 'package:fireboy_and_watergirl/config/sockets/models/player_movement.dart';
+import 'package:fireboy_and_watergirl/providers/movement_provider.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/services.dart' show LogicalKeyboardKey;
 import 'package:fireboy_and_watergirl/characters/character_animation.dart';
@@ -12,21 +14,27 @@ class WaterGirlAnimation extends CharacterAnimation {
 
   @override
   void jumpFromKeyEvent(Set<LogicalKeyboardKey> keysPressed) {
+    final playerMovementController = ref.read(providerPlayerMovement.notifier);
     if (keysPressed.contains(LogicalKeyboardKey.arrowUp) && onGround) {
       jump();
+      playerMovementController.sendMove(PlayerMovementAction.jump, position.x, position.y);
     }
   }
 
   @override
   void moveFromKeyEvent(Set<LogicalKeyboardKey> keysPressed) {
+    final playerMovementController = ref.read(providerPlayerMovement.notifier);
     if (keysPressed.contains(LogicalKeyboardKey.arrowLeft)) {
       animation = walkLeftAnimation;
       velocity.x = -speed;
+      playerMovementController.sendMove(PlayerMovementAction.moveLeft, position.x, position.y);
     } else if (keysPressed.contains(LogicalKeyboardKey.arrowRight)) {
       animation = walkRightAnimation;
       velocity.x = speed;
+      playerMovementController.sendMove(PlayerMovementAction.moveRight, position.x, position.y);
     } else {
       animation = idleAnimation;
+      playerMovementController.sendMove(PlayerMovementAction.idle, position.x, position.y);
     }
   }
 }
