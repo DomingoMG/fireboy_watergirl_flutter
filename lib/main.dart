490386ef94f767430568ef30c279b688366de98c
DomingoMG <domingomg97@gmail.com>
2025-03-17 21:54:27 +0000

    Refactored main method to support async initialization and fullscreen mode.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flame/flame.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flame_riverpod/flame_riverpod.dart';
 import 'package:flutter/material.dart';
@@ -9,7 +10,10 @@ import 'package:fireboy_and_watergirl/fireboy_and_watergirl_game.dart';
 final gameInstance = FireBoyAndWaterGirlGame();
 final gameWidgetKey = GlobalKey<RiverpodAwareGameWidgetState>();
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Flame.device.fullScreen();
+  await Flame.device.setLandscape();
   runApp(ProviderScope(
     child: MaterialApp(
       home: RiverpodAwareGameWidget(
