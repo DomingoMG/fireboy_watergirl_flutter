9911ada70d6c19bd97d42cd5f44bcef711dc0f69
DomingoMG <domingomg97@gmail.com>
2025-03-22 18:53:14 +0000

    Initialize AudioManager on app startup in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flame/flame.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flame_riverpod/flame_riverpod.dart';
 import 'package:flutter/material.dart';
+import 'package:fireboy_and_watergirl/config/audio/audio_manager.dart';
 import 'package:fireboy_and_watergirl/overlays/lobby_menu.dart';
 import 'package:fireboy_and_watergirl/overlays/waiting_player.dart';
 import 'package:fireboy_and_watergirl/overlays/main_menu.dart';
@@ -14,6 +15,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Flame.device.fullScreen();
   await Flame.device.setLandscape();
+  await AudioManager.init();
   runApp(ProviderScope(
     child: MaterialApp(
       home: RiverpodAwareGameWidget(
